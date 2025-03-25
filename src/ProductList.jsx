31ac14e310c0cb564427d4367597079fe9ddf55f
Livlau <kodekode2021@gmail.com>
2025-03-24 23:16:02 -0400

    add product price and description to the product grid

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -288,13 +288,14 @@ function ProductList({ onHomeClick }) {
                 <div className="product-grid">
                     {plantsArray.map((category, index) => (
                         <div key={index}>
-                            <h1><div>{category.category}</div></h1>
+                            <h1><div className="product-category">{category.category}</div></h1>
                             <div className="product-list">
                                 {category.plants.map((plant, plantIndex) => (
                                 <div className="product-card" key={plantIndex}>
                                     <img className="product-image" src={plant.image} alt={plant.name} />
                                     <div className="product-title">{plant.name}</div>
-                                    {/*Similarly like the above plant.name show other details like description and cost*/}
+                                    <div className="product-price">{plant.cost}</div>
+                                    <div className="product-description">{plant.description}</div>
                                     <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                                 </div>
                                 ))}
